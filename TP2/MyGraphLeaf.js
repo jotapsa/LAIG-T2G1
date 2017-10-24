b7101327cac03cd3fca4a9993a44d4f8944821bd
Joao Pedro Teixeira Pereira de Sa <up201506252@B02AY0601.fe.up.pt>
2017-10-24 11:24:11 +0100

    TP2 folder

---
+++ TP2/MyGraphLeaf.js
@@ -0,0 +1,57 @@
+/**
+ * MyGraphLeaf class, representing a leaf in the scene graph.
+ * @constructor
+**/
+
+function MyGraphLeaf(graph, type, args,argsP) {
+    this.reader = new CGFXMLreader();
+    this.type = type;
+    this.args = args;
+    this.argsP = argsP;
+
+    this.object = null;
+
+    switch(this.type){
+        case 'rectangle':
+            if(this.args.length != 4){
+                console.log("error: number of arguments");
+            }
+            else{
+                this.object = new Rectangle(graph.scene, this.args[0], this.args[1], this.args[2], this.args[3]);
+            }
+            break;
+        case 'cylinder':
+            if(this.args.length != 7){
+                console.log("error: number of arguments");
+            }
+            else{
+                this.object = new Cylinder(graph.scene, this.args[0], this.args[1], this.args[2], this.args[3], this.args[4], this.args[5], this.args[6]);
+            }
+            break;
+        case 'sphere':
+            if(this.args.length != 3){
+                console.log("error: number of arguments");
+            }
+            else{
+                this.object = new Sphere(graph.scene, this.args[0], this.args[1], this.args[2]);
+            }
+            break;
+        case 'triangle':
+            if(this.args.length != 9){
+                console.log("error: number of arguments");
+            }
+            else{
+                this.object = new Triangle(graph.scene, this.args[0], this.args[1], this.args[2], this.args[3], this.args[4], this.args[5], this.args[6], this.args[7], this.args[8]);
+            }
+            break;
+        case 'patch':
+            if(this.args.length != 2){
+                console.log("error: number of arguments");
+            }
+            else
+                this.object = new Patch(graph.scene,this.args,this.argsP);
+            break;
+        default:
+            break;
+    }
+}
---
+++ TP2/Rectangle.js
@@ -0,0 +1,74 @@
+var X = 0;
+var Y = 1;
+var Z = 2;
+
+/**
+ * Rectangle
+ * @constructor
+ */
+ function Rectangle(scene, x1, y1, x2, y2) {
+ 	CGFobject.call(this,scene);
+  
+    this.topLeftVertex = [];
+    this.topLeftVertex.push(x1, y1, 0);
+
+    this.botRightVertex = [];
+    this.botRightVertex.push(x2, y2, 0);
+
+ 	this.minS = 0;
+ 	this.maxS = 1;
+ 	this.minT = 0;
+ 	this.maxT = 1;
+ 	
+ 	this.initBuffers();
+ 	
+ };
+
+ Rectangle.prototype = Object.create(CGFobject.prototype);
+ Rectangle.prototype.constructor = Rectangle;
+
+ Rectangle.prototype.initBuffers = function() {
+ 	this.vertices = [
+ 	this.topLeftVertex[X], this.topLeftVertex[Y], 0,
+ 	this.topLeftVertex[X], this.botRightVertex[Y], 0,
+ 	this.botRightVertex[X], this.botRightVertex[Y], 0,
+ 	this.botRightVertex[X], this.topLeftVertex[Y], 0,
+ 	];
+
+ 	this.texCoords = [
+    this.minS, this.minT,
+    this.minS, this.maxT,
+    this.maxS, this.maxT,
+    this.maxS, this.minT,
+ 	];
+
+ 	
+    this.originalTexCoords = this.texCoords.slice();
+
+ 	this.indices = [
+ 	0, 1, 2, 
+ 	0, 2, 3
+ 	]; 	
+
+ 	this.normals = [
+      0, 0, 1,
+      0, 0, 1,
+      0, 0, 1,
+      0, 0, 1,
+    ]
+
+    this.originalTexCoords = this.texCoords.slice();
+
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    
+ 	this.initGLBuffers();
+ };
+
+ Rectangle.prototype.scaleTexCoords = function(sFactor, tFactor) {
+    for (i = 0; i<this.texCoords.length; i+=2) {
+        this.texCoords[i] = this.originalTexCoords[i] / sFactor;
+        this.texCoords[i+1] = this.originalTexCoords[i+1] / tFactor;
+    }
+
+    this.updateTexCoordsGLBuffers();
+};
