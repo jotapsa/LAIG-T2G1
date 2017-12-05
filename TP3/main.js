41cfc1b38b40f5c1cb70ecdec0556bed9bc05d67
Bernardo Barbosa <up201503477@fe.up.pt>
2017-12-05 12:49:34 +0000

    add piece model

--- TP3/main.js
+++ TP3/main.js
@@ -34,7 +34,7 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor)
 
-	var filename=getUrlVars()['file'] || "LAIG_TP1_LSX_T2_G01_v02.xml";
+	var filename=getUrlVars()['file'] || "LAIG_TP3_LSX_T2_G01_v02.xml";
 
 	// create and load graph, and associate it to scene.
 	// Check console for loading errors
