87f549cc65d9f1acd79eb27fc56100c5585588d4
João Sá <up201506252@fe.up.pt>
2017-12-22 19:38:30 +0000

    Deleting piece from board if it gets eaten

--- TP3/Draughts/DraughtMap.js
+++ TP3/Draughts/DraughtMap.js
@@ -34,6 +34,10 @@ DraughtMap.prototype.makeMove = function (move){
   startingPos = move.getStartingPos();
   finalPos = move.getFinalPos();
 
+  if(Math.abs(finalPos[0]-startingPos[0]) == 2 && Math.abs(finalPos[1]-startingPos[1]) == 2){
+    this.capturePiece(move);
+  }
+
   this.map[finalPos[0]][finalPos[1]] = this.map[startingPos[0]][startingPos[1]];
   this.map[startingPos[0]][startingPos[1]] = CELL.EMPTY_SQUARE;
 
@@ -44,6 +48,32 @@ DraughtMap.prototype.makeMove = function (move){
   else if(finalPos[0] == (this.sizeN-1) && this.map[finalPos[0]][finalPos[1]] == CELL.WHITE_PIECE){
     this.map[finalPos[0]][finalPos[1]] = CELL.WHITE_KING;
   }
+}
+
+DraughtMap.prototype.capturePiece = function(move){
+  let startingPos, intermediatePos, finalPos;
+  let delta;
+  startingPos = move.getStartingPos();
+  finalPos = move.getFinalPos();
+
+  delta = [finalPos[0]-startingPos[0], finalPos[1]-startingPos[1]];
+  intermediatePos = [startingPos[0] + delta[0]/2, startingPos[1] + delta[1]/2];
+
+  switch(this.map[startingPos[0]][startingPos[1]]){
+    case CELL.BLACK_KING:
+    case CELL.BLACK_PIECE:{
+      //maybe whitepieces--; ?
+    }
+    break;
+    case CELL.WHITE_KING:
+    case CELL.WHITE_PIECE:{
+      //maybe blackpieces--; ?
+    }
+    default:
+    break;
+  }
+
+  this.map[intermediatePos[0]][intermediatePos[1]] = CELL.EMPTY_SQUARE;
 }
 
 DraughtMap.prototype.resetMap = function (){
